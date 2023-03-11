c6e47ba1801a99abbd9dbaa4a107fbcef7730538
grant <grant@DevBox.grant.virtualbox.org>
2023-03-11 15:58:52 -0500

    create project shell

---
+++ src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.hpp"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    
+    return 0;
+}
---
+++ src/include/Test.hpp
@@ -0,0 +1,18 @@
+#ifndef DSIMLOG_TEST
+#define DSIMLOG_TEST
+
+class Test
+{
+public:
+
+    Test();
+
+    ~Test() = default;
+
+private:
+
+    int x { 0 };
+
+};
+
+#endif
